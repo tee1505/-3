bcc7b43c2e960e9ec051bafa064e5de5769434b9
tee1505 <mon.motoo01@gmail.com>
2025-08-21 13:40:45 +0700

    Update Task5.sql

--- Task5.sql
+++ Task5.sql
@@ -1,3 +1,7 @@
 SELECT categoryName, ProductName, UnitPrice
 FROM Products,Categories
 WHERE Products.CategoryID = Categories.CategoryID
+
+SELECT categoryName, ProductName, UnitPrice
+FROM Products as p,Categories as c
+WHERE p.CategoryID = c.CategoryID
