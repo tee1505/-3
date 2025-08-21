3784024763e252131b2f738c024a5f8f80e70599
tee1505 <mon.motoo01@gmail.com>
2025-08-21 13:45:52 +0700

    Update Task5.sql

--- Task5.sql
+++ Task5.sql
@@ -5,3 +5,19 @@ WHERE Products.CategoryID = Categories.CategoryID
 SELECT categoryName, ProductName, UnitPrice
 FROM Products as p,Categories as c
 WHERE p.CategoryID = c.CategoryID
+
+SELECT categoryName, ProductName, UnitPrice
+FROM Products as p join Categories as c
+on p.CategoryID = c.CategoryID
+
+SELECT categoryName, ProductName, UnitPrice
+FROM Products as p,Categories as c
+WHERE p.CategoryID = c.CategoryID
+and CategoryName = 'seafood'
+
+--แนะนำให้ใช้อันนี้
+SELECT categoryName, ProductName, UnitPrice
+FROM Products as p join Categories as c
+on p.CategoryID = c.CategoryID
+where CategoryName = 'seafood'
+
